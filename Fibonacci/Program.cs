fcda2e31a86e6a3d823d9c7c29ebf3a939722294
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-03-17 15:57:02 +0100

    to finish

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -4,9 +4,29 @@ namespace Fibonacci
 {
     class Program
     {
+
+        private static int Fibonacci(int num){
+            int fibo = 1;
+ 
+            
+            if(fibo == 1){
+                fibo = 1;
+            }
+             else{
+                 fibo = fibo + Fibonacci(num-1);
+             }
+
+            return 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fibonacci!");
+            int n = 8;
+
+            
+            
+            Console.WriteLine("out->"+Convert.ToInt64(Fibonacci(n)));
+
         }
     }
 }
