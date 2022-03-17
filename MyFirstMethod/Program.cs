ffe001b014622174aae86a241ee9125869b3d7de
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-03-17 15:21:38 +0100

    XML Documentation

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,19 +4,31 @@ namespace MyFirstMethod
 {
     class Program
     {
-
+        /// <summary>
+        /// Counts from 1 to 10
+        /// </summary>
+        /// <returns>None</returns>
         private static async void CountTo10(){
             for(int i = 1; i <= 10; i++){
                 Console.WriteLine(i);
             }
         }
 
+        /// <summary>
+        /// Gets a number and counts from 1 to it
+        /// </summary>
+        /// <param name="n">the number to reach</param>
+        /// <returns>None</returns>
         private static async void CountToN(int n){
             for(int i = 1; i <= n; i++){
                 Console.WriteLine(i);
             }
         }
 
+        /// <summary>
+        /// Main with summon of other methods
+        /// </summary>
+        /// <param name="args">Received Arguments</param>
         static void Main(string[] args)
         {
             CountToN(5);
